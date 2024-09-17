8a2684c91bfb57837a61022fbddefba25b3008a7
Vladyslav Korneiko <aquillaboost@gmail.com>
2024-09-17 20:53:47 +0300

    v2(simple)

---
+++ calc/calc.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    try {
+        display.value = math.evaluate(display.value);
+    } catch (error) {
+        display = 'Ошибка';
+    }
+}
